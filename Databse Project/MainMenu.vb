e84453c11aa56f54045762467d354c6cd8ac0f83
sebja <sebjackson2009@gmail.com>
2026-03-02 21:19:18 +0000

    INSERTING INTO DATABASES Added SQL logic to create a new customer, account and make a transaction. Changed the "OpenMode" enumeration to "Selection" and moved it to the MainMenu form. Added an "EnterCreateDetails" form to enter values when creating a new customer/account/transaction. Changed the "Insert" function in the "DatabaseControls" module to take an array of objects rather than strings for the "fieldValues" array and migrated to using parameters rather than concatenation to form the insert query.

--- Databse Project/MainMenu.vb
+++ Databse Project/MainMenu.vb
@@ -1,27 +1,46 @@
 ﻿Public Class MainMenu
+
+    Enum Selection
+        ViewBalance
+        ViewCustomer
+        ViewTransaction
+        CreateCustomer
+        CreateAccount
+        MakeTransaction
+    End Enum
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
         Form1.Show()
     End Sub
 
     Private Sub ViewBalanceButton_Click(sender As Object, e As EventArgs) Handles ViewBalanceButton.Click
-        EnterDetails.SetOpenMode(EnterDetails.OpenMode.ViewBalance)
-        EnterDetails.Show()
+        EnterSearchDetails.SetOpenMode(Selection.ViewBalance)
+        EnterSearchDetails.Show()
     End Sub
 
     Private Sub ViewCustomerButton_Click(sender As Object, e As EventArgs) Handles ViewCustomerButton.Click
-        EnterDetails.SetOpenMode(EnterDetails.OpenMode.ViewCustomer)
-        EnterDetails.Show()
+        EnterSearchDetails.SetOpenMode(Selection.ViewCustomer)
+        EnterSearchDetails.Show()
     End Sub
 
     Private Sub ViewTransactionButton_Click(sender As Object, e As EventArgs) Handles ViewTransactionButton.Click
-        EnterDetails.SetOpenMode(EnterDetails.OpenMode.ViewTransaction)
-        EnterDetails.Show()
+        EnterSearchDetails.SetOpenMode(Selection.ViewTransaction)
+        EnterSearchDetails.Show()
     End Sub
 
     Private Sub MakeTransactionButton_Click(sender As Object, e As EventArgs) Handles MakeTransactionButton.Click
-        EnterDetails.SetOpenMode(EnterDetails.OpenMode.MakeTransaction)
-        EnterDetails.Show()
+        EnterCreateDetails.SetOpenMode(Selection.MakeTransaction)
+        EnterCreateDetails.Show()
+    End Sub
+    Private Sub CreateCustomerButton_Click(sender As Object, e As EventArgs) Handles CreateCustomerButton.Click
+        EnterCreateDetails.SetOpenMode(Selection.CreateCustomer)
+        EnterCreateDetails.Show()
+    End Sub
+
+    Private Sub CreateAccountButton_Click(sender As Object, e As EventArgs) Handles CreateAccountButton.Click
+        EnterCreateDetails.SetOpenMode(Selection.CreateAccount)
+        EnterCreateDetails.Show()
     End Sub
 
     Private Sub MainMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -29,5 +48,6 @@
         DatabaseControls.LoadDatabase()
 
     End Sub
+
 
 End Class
