2357ddfa8f6379b14db04acc0297789e7471586b
sebja <sebjackson2009@gmail.com>
2026-03-02 20:19:25 +0000

    Added IntelliSense comments to DatabaseControls

--- Databse Project/DatabaseControls.vb
+++ Databse Project/DatabaseControls.vb
@@ -5,6 +5,9 @@ Imports System.Environment
 Imports System.IO
 Imports ADOX
 
+''' <summary>
+''' Controls for the bank database
+''' </summary>
 Module DatabaseControls
 
     Const dataBaseProvider As String = "Provider=Microsoft.ACE.OLEDB.16.0;"
@@ -18,6 +21,9 @@ Module DatabaseControls
 
     Dim path As String = IO.Directory.GetParent(IO.Directory.GetParent(My.Application.Info.DirectoryPath).ToString).ToString
 
+    ''' <summary>
+    ''' Runs when program opens to check whether the bank database already exists and if not creates it.
+    ''' </summary>
     Public Sub StartChecks()
 
         If Not IO.File.Exists(path & "\BankDatabase.accdb") Then
@@ -82,6 +88,11 @@ Module DatabaseControls
 
     End Sub
 
+    ''' <summary>
+    ''' Runs a select query and displays the output in a DataGridView.
+    ''' </summary>
+    ''' <param name="sqlQuery">The query to run.</param>
+    ''' <param name="dataGrid">The DataGridView to display the data in.</param>
     Public Sub RunSelectQuery(sqlQuery As String, dataGrid As DataGridView)
         If databaseLoaded Then
 
@@ -106,6 +117,10 @@ Module DatabaseControls
 
     End Sub
 
+    ''' <summary>
+    ''' Runs an SQL query.
+    ''' </summary>
+    ''' <param name="sqlQuery">The query to run.</param>
     Public Sub RunQuery(sqlQuery As String)
 
         connection.Open()
@@ -117,6 +132,12 @@ Module DatabaseControls
         connection.Close()
     End Sub
 
+    ''' <summary>
+    ''' Runs an insert query based on user specified values.
+    ''' </summary>
+    ''' <param name="tableName">The table in the database to insert into.</param>
+    ''' <param name="fieldNames">A string array containing the names of the fields (in order) to insert into in the query.</param>
+    ''' <param name="fieldValues">The values to insert into each of the fields specified in the "fieldNames" array.</param>
     Public Sub Insert(tableName As String, fieldNames As String(), fieldValues As String())
 
         If databaseLoaded Then
@@ -140,6 +161,9 @@ Module DatabaseControls
         End If
     End Sub
 
+    ''' <summary>
+    ''' Gives the user a dialogue box from which to select an access database.
+    ''' </summary>
     Public Sub LoadDatabaseFromDialog()
 
         Dim myOpenFileDialog As New OpenFileDialog
@@ -157,6 +181,9 @@ Module DatabaseControls
 
     End Sub
 
+    ''' <summary>
+    ''' Loads the bank database
+    ''' </summary>
     Public Sub LoadDatabase()
         myDatabasePath = path & "\BankDatabase.accdb"
         connection = New OleDbConnection(dataBaseProvider & "Data Source=" & myDatabasePath)
